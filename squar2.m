9ffc1e314c62cf5b0ebe8d746c22f4a215f90747
Yung-Hsuan <71300463+Yung-Hsuan@users.noreply.github.com>
2020-12-25 14:44:45 +0800

    Add files via upload

---
+++ squar2.m
@@ -0,0 +1,20 @@
+function squar2(n)
+B=0;
+for ii=1:n
+    B=B+ii^2-(ii-1);
+end
+if n/2==ceil(n/2)
+    for ii=2:2:n
+        B=B+ii^2;
+        B=B+(ii-1)^2+1;
+    end
+else
+    for ii=3:2:n
+        B=B+ii^2;
+        B=B+(ii-1)^2+1;
+    end
+end
+disp('對角線及反對角線之總和:')
+disp(B)
+
+end
